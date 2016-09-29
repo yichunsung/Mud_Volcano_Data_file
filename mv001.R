5c961ed48f24f1b0a0dcd728e97e4692cb486495
yichunsung <cd12631@gmail.com>
2016-09-29 17:21:29 +0800

    update about mv001.R

--- mv001.R
+++ mv001.R
@@ -2,22 +2,22 @@
 
 setwd('c:/R/Mud_Volcano_Data_file') #Change the wd path for yourself!!!!
 Sys.setlocale(category = "LC_ALL", locale = "")
-mv_data <- read.csv("mv001.csv", header= T, sep=",") #loading Mud Volcano Data 
+mv_data <- read.csv("mv001.csv", header= T, sep=",") #loading Mud Volcano Data
 
-# Change the Data.Frame header name 
-names(mv_data)#print the row 
-names(mv_data)[1:12] <- 
-    c("sample_day", 
-      "site", 
-      "Ar_conc", 
-      "N2_conc", 
-      "CO_conc", 
-      "CH4_conc", 
-      "CO2_conc", 
-      "C2H6_conc", 
-      "C3H8_conc", 
-      "He_conc", 
-      "H2_conc", 
+# Change the Data.Frame header name
+names(mv_data)#print the row
+names(mv_data)[1:12] <-
+    c("sample_day",
+      "site",
+      "Ar_conc",
+      "N2_conc",
+      "CO_conc",
+      "CH4_conc",
+      "CO2_conc",
+      "C2H6_conc",
+      "C3H8_conc",
+      "He_conc",
+      "H2_conc",
       "O2_conc")
 WD_MV <- subset(mv_data, site=="WD")
 WD_MV_CH4 <- subset(WD_MV, CH4_conc!="")
@@ -35,6 +35,8 @@ ggplot(WD_MV_CH4, aes(x=CH4_conc))+geom_histogram(binwidth = 3, fill="red", colo
 
 
 #pd air station data for Methane
+setwd('c:/R/Mud_Volcano_Data_file') #Change the wd path for yourself!!!!
+Sys.setlocale(category = "LC_ALL", locale = "")
 pd_air <- read.csv("pd2015.csv", header = T, sep=",") #Loading pd station 2015 Air Data
 names(pd_air)
 names(pd_air)[1:3] <- c("day", "site", "no")
@@ -42,15 +44,65 @@ summary(pd_air)
 pd_CH4 <- subset(pd_air, no=="CH4")#subset•¸!
 pd_CH4_draw <- subset(pd_CH4, X00!="")
 summary(pd_CH4)
-x00_new=as.numeric(as.vector(pd_CH4_draw$X00))
+x00_new <- as.numeric(as.vector(pd_CH4_draw$X00))
 pd_CH4_new<- cbind(pd_CH4_draw, x00_new)
 pd_CH4_new1 <- subset(pd_CH4_new, x00_new!="NA")
 day_new <- c(1:360)
 pd_CH4_new2<- cbind(pd_CH4_new1, day_new)
 pd_CH4_new2$asDate <- as.Date(pd_CH4_new2$day, "%Y/%m/%d")
 
-# draw
+#=----------------------------------------------------------------------------------------------=
+#=                                          TEST                                                =
+#=                                                                                              =
+#-----------------------------------------------------------------------------------------------=
+
+newFrameofTestMV <- pd_CH4_draw[4:27]
+colnames(newFrameofTestMV) <- c(0:23)
+g<-c()
+newFrameCH4 <- data.frame()
+for (i in 1:ncol(newFrameofTestMV)){
+data1 <- newFrameofTestMV[, i]
+  for (j in 1:nrow(newFrameofTestMV)){
+    data2 <- data1[j]
+    data3 <- as.numeric(as.vector(data2))
+      if(data3 = "NA") {
+        data3 <-c(0)
+        g[j] <- cbind(data3)
+      }else{
+        g[j] <- cbind(data3)
+
+      }
+   }
+
+  }
+
+
+
+# Test
+test0 <- pd_CH4_draw[, 4]
+testA <- subset(test0, "*")
+colNum <- as.integer(testA)
+test2 <- as.numeric(as.vector(pd_CH4_draw[1,4]))
+
+#=----------------------------------------------------------------------------------------------=
+#=                                          TEST                                                =
+#=                                                                                              =
+#-----------------------------------------------------------------------------------------------=
+
+
+
+
+# draw with ggplot2
 install.packages("ggplot2")
 library(ggplot2)
 
-ggplot(pd_CH4_new2, aes(x=asDate, y=x00_new))+geom_line()+geom_point(size=2)+ggtitle("PD Air Station for Methane")+xlab("Day")+ylab("conc. (ppm)")
+ggplot(pd_CH4_new2, aes(x=asDate, y=x00_new))+geom_line(colour="red")+geom_point(size=1, colour="black")+
+  ggtitle("PD Air Station for Methane")+xlab("Day")+ylab("conc. (ppm)")
+
+# draw with plotly
+install.packages("plotly")
+library(plotly)
+
+plotly_pdStatMethane <- plotly
+
+
